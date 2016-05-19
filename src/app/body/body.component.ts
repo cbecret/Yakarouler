7855c0d827dab9665cddcc059a6e10f3ee839618
Cyril Bécret <cyril.becret@gmail.com>
2016-05-19 09:29:01 +0200

    bootstrapping

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -2,13 +2,17 @@ import { Component, OnInit } from '@angular/core';
 import { RechercheComponent } from '../recherche/recherche.component';
 import { BannerComponent } from '../banner/banner.component';
 import { MavoitureComponent } from '../mavoiture/mavoiture.component';
+import { OffresComponent } from '../offres/offres.component';
+import { NewsletterComponent } from '../newsletter/newsletter.component';
+import { SocialComponent } from '../social/social.component';
 
 @Component({
   moduleId: module.id,
   selector: 'yaka-body',
   templateUrl: 'body.component.html',
   styleUrls: ['body.component.css'],
-  directives: [RechercheComponent, BannerComponent, MavoitureComponent]
+  directives: [RechercheComponent, BannerComponent, MavoitureComponent, OffresComponent,
+              NewsletterComponent, SocialComponent]
 })
 export class BodyComponent implements OnInit {
 
